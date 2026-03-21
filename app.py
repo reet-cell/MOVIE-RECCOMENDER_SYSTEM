35d8070390c32f66780f2665f197f9d80bdc7ca5
REET JAIN <reet.jain18394@sakec.ac.in>
2026-03-21 13:42:02 +0530

    Clean commit without large files

---
+++ app.py
@@ -0,0 +1,79 @@
+
+import pandas as pd
+import streamlit as st
+import pickle
+import os
+import requests
+
+# ---------------- DOWNLOAD FILE FUNCTION ----------------
+def download_file(url, filename):
+    if not os.path.exists(filename):
+        r = requests.get(url)
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+
+# ---------------- DOWNLOAD FILES ----------------
+download_file("https://drive.google.com/uc?id=1w9X0e7EXcW-zVl5Yp7st85blIUotc0t7", "similarity.pkl")
+download_file("https://drive.google.com/uc?id=1EMhqpUsfSO2iGOUO5432es8eRuTHqg3Q", "movie_list.pkl")
+
+# ---------------- LOAD FILES ----------------
+movie = pickle.load(open('movie_list.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+movie = pd.DataFrame(movie)
+
+# ---------------- UI ----------------
+st.title("Movie Recommender System")
+
+selected_movie_name = st.selectbox('Select movie', movie['title'].values)
+
+# ---------------- FETCH POSTER ----------------
+def fetch_poster(movie_id):
+    response = requests.get(
+        "https://api.themoviedb.org/3/movie/{}?api_key=f07164adf1a7def0170eeafeeb6bb25a".format(movie_id)
+    )
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+# ---------------- RECOMMEND FUNCTION ----------------
+def recommend(movie_name):
+    movie_index = movie[movie['title'] == movie_name].index[0]
+    distances = similarity[movie_index]
+
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movie = []
+    recommended_movie_posters = []
+
+    for i in movies_list:
+        movie_id = movie.iloc[i[0]].movie_id
+        recommended_movie.append(movie.iloc[i[0]].title)
+        recommended_movie_posters.append(fetch_poster(movie_id))
+
+    return recommended_movie, recommended_movie_posters
+
+# ---------------- BUTTON ----------------
+if st.button('Show Recommendation'):
+    names, posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
